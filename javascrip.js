177cf4faebc851040b2e926f12bb241cb26a6171
Nick <nickmeza@upeu.edu.pe>
2019-04-09 20:22:03 -0500

    commit

---
+++ javascrip.js
@@ -0,0 +1,13 @@
+let nom= document.getElementById("cliente").innerHTML;
+let dni= document.getElementById("dni").innerHTML;
+let fecha= "9/04/2019"
+let ubicacion= document.getElementById("ubicacion").innerHTML;
+let tipo= document.getElementById("tipo").innerHTML;
+let cant=document.getElementById("personas").innerHTML;
+function guardar(){
+
+
+}
+function agregar_fila1(){
+hdp
+}
